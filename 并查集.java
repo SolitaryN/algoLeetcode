94db3b489e5dbf6b7f05f6fe9c09980493956086
solitary <wtfuwakeup8@gmail.com>
2025-03-06 21:52:01 +0800

    补充优化并查集

--- 并查集.java
+++ 并查集.java
@@ -6,34 +6,38 @@ class UnionFind{
     int n;//节点的个数，初始化每一个节点都是一个单独的连通分量
     int setCount;//连通分量的数目
     public UnionFind(int n){
-        this.size=new int[n];
-        this.parent=new int[n];
-        this.n=n;
-        this.setCount=n;
+        this.size = new int[n];
+        this.parent = new int[n];
+        this.n = n;
+        this.setCount = n;
         Arrays.fill(size,1);
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-        }
+        Arrays.fill(parent, -1);
     }
 
+    // 这里find的时候，如果已经是root节点，则返回否则递归
     public int find(int x){
-        return parent[x]==x?x:find(parent[x]);
+        if (parent[x] == -1) {
+            return x; // 已经是根节点
+        }
+        // 路径压缩
+        parent[x] = find(parent[x]);
+        return parent[x];
     }
 
-    public boolean unit(int x,int y){
-        x=find(x);
-        y=find(y);
-        if(x==y){
+    public boolean union(int x,int y){
+        x = find(x);
+        y = find(y);
+        if(x == y){
             return false;
         }
         if(size[x]<size[y]){
-            int tem=x;
-            x=y;
-            y=tem;
+            int tem = x;
+            x = y;
+            y = tem;
         }
 
-        parent[y]=x;
-        size[x]+=size[y];
+        parent[y] = x;
+        size[x] += size[y];
         --setCount;
         return true;
     }
