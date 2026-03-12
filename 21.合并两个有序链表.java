cec0c3632616cc358dce3d59c154b2b46090d8e4
zoushuaiyi <zoushuaiyi@travelsky.com.cn>
2026-03-12 09:58:58 +0800

    前一天的

--- 21.合并两个有序链表.java
+++ 21.合并两个有序链表.java
@@ -19,6 +19,13 @@ import java.util.Currency;
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+  class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
 class Solution {
 
     /*
